549a7fec83290dcd1ec90bb49360156b36017392
tmori <tk10314@gmail.com>
2021-05-17 19:14:09 +0900

    5/17 更新

---
+++ ABC/ABC165/b165d.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+using namespace std;
+// using namespace atcoder;
+typedef long long ll;
+const ll MOD = 1000000007;
+
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define coutALL(x) for(auto i=x.begin();i!=--x.end();i++) cout<<*i<<" ";cout<<*--x.end()<<endl;
+#define coutVEC2(x) rep(j, x.size()) {auto y=x.at(j); coutALL(y);}
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main(){
+	ll a,b,n;
+	cin>>a>>b>>n;
+	ll x=min(b-1, n);
+	cout<<a*x/b-x/b*a<<endl;
+
+	return 0;
+}
